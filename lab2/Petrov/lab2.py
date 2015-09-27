f7e2d046675adb156f39f21a04af1c50bdfdf580
fromfall <chemhist@yandex.ru>
2015-09-27 16:11:08 +0300

    Update lab2.py

--- lab2/Petrov/lab2.py
+++ lab2/Petrov/lab2.py
@@ -1,8 +1,10 @@
 from sys import stdin
 
 def sieve(n):
-    a = [True for i in range(n + 1)] 
-    for i in range(2, int(math.sqrt(n)) + 1):
+    a = [True for i in range(n + 1)]
+    a[0] = False
+    a[1] = False
+    for i in range(2, n + 1):
         j = i * 2
         while j <= n:
             a[j] = False
