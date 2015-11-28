18299e0b26b4ef4a915911787b17c591157085d4
fromfall <chemhist@yandex.ru>
2015-11-28 20:53:38 +0300

    Update lab5.py

--- lab5/Petrov/lab5.py
+++ lab5/Petrov/lab5.py
@@ -2,22 +2,30 @@ from random import *
 from sys import stdin
 a = list(map(int, stdin.readline().split()))
  
-def quickSort(a):
-    if len(a) <= 1:
-        return a
-    less = []
-    more = []
-    p = choice(a)
-    for i in a:
-        if i < p:
-            less.append(i)
-        if i > p:
-            more.append(i)
-    less = quickSort(less)
-    more = quickSort(more)
-    return less + [p] * a.count(p) + more
-    
-b = quickSort(a)
+def partition(l, r):
+    x = a[randint(l, r)]
+    i = l
+    j = r
+    while 1:
+        while a[i] < x:
+            i += 1
+        while a[j] > x:
+            j -= 1
+        if i < j:
+            a[i], a[j] = a[j], a[i]
+            i += 1
+            j -= 1
+        else:
+            return j
+           
+def quickSort(l, r):
+    if l < r:
+        q = partition(l, r)
+        quickSort(l, q)
+        quickSort(q + 1, r)
+    return a
+   
+b = quickSort(0, len(a) - 1)
  
 for i in range(len(b)):
     print(b[i], end = ' ')
