00f733cda98728d33ae9e250a128d4528885488c
fromfall <chemhist@yandex.ru>
2015-12-24 17:51:29 +0300

    Update test15.py

--- lab15/test15.py
+++ lab15/test15.py
@@ -6,7 +6,8 @@ class Tester(unittest.TestCase):
     def test_example(self):    
         n = 9
         root = 3
-        g = [[8], [7], [5, 6, 8], [7, 5], [7], [3, 2], [2], [1, 4, 3], [0, 2]]
+        v = [[1, 7], [4, 7], [7, 3], [5, 3], [2, 5], [6, 2], [8, 2], [0, 8]]
+        g = LCA.list_of_edges(n, v)
         lca = LCA(g, root)
         self.assertEqual(lca.lca(1, 8), 3)
         self.assertEqual(lca.lca(6, 2), 2)
@@ -16,27 +17,29 @@ class Tester(unittest.TestCase):
     def test_simple(self):    
         n = 10
         root = 1
-        g = [[9], [2, 3], [1, 4, 5], [1, 6, 7], [2], [2], [3, 8, 9], [3], [6], [0, 6]]
+        v = [[1, 2], [1, 3], [2, 4], [2, 5], [3, 6], [3, 7], [6, 8], [6, 9], [9, 0]]
+        g = LCA.list_of_edges(n, v)
         lca = LCA(g, root)
-        self.assertEqual(lca.lca(8, 9), 6)
-        self.assertEqual(lca.lca(6, 7), 3)
-        self.assertEqual(lca.lca(3, 2), 1)
-        self.assertEqual(lca.lca(8, 7), 3)
-        self.assertEqual(lca.lca(6, 9), 6)
-        self.assertEqual(lca.lca(4, 5), 2)
+        self.assertEqual(lca.lca(0, 7), 3)
+        self.assertEqual(lca.lca(5, 3), 1)
+        self.assertEqual(lca.lca(7, 3), 3)
+        self.assertEqual(lca.lca(8, 0), 6)
+        self.assertEqual(lca.lca(9, 6), 6)
 
     def test_another(self):    
         n = 15
         root = 5
-        g = [[13], [3], [3], [1, 2, 4], [3, 9], [6, 9, 10], [5, 7, 8], [6], [6], [4, 5], 
-            [5, 11, 12], [10], [10, 13], [0, 12, 14], [13]]
+        v = [[5, 6], [5, 9], [5, 10], [6, 7], [6, 8], [9, 4], [4, 3], [3, 2], [3, 1],
+                    [10, 11], [10, 12], [12, 13], [13, 14], [13, 0]]
+        g = LCA.list_of_edges(n, v)
         lca = LCA(g, root)
-        self.assertEqual(lca.lca(0, 14), 13)
-        self.assertEqual(lca.lca(13, 11), 10)
-        self.assertEqual(lca.lca(11, 0), 10)
-        self.assertEqual(lca.lca(3, 7), 5)
-        self.assertEqual(lca.lca(7, 8), 6)
-        self.assertEqual(lca.lca(9, 10), 5)
+        self.assertEqual(lca.lca(3, 4), 4)
+        self.assertEqual(lca.lca(14, 0), 13)
+        self.assertEqual(lca.lca(11, 10), 10)
+        self.assertEqual(lca.lca(2, 3), 3)
+        self.assertEqual(lca.lca(11, 13), 10)
+        self.assertEqual(lca.lca(0, 11), 10)
+           
 
 if __name__ == '__main__':
     unittest.main()
